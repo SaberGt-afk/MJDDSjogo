40f244a8c48f0944b237e66d95086c78595995cb
SaberGt-afk <gtsaber0000@gmail.com>
2025-04-24 20:12:39 +0100

    teste

--- Assets/scrpits menu/Clickable.cs
+++ Assets/scrpits menu/Clickable.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Clickable : MonoBehaviour
+public class clickable : MonoBehaviour
 {
     [Range(0f, 1f)]
     public float alphaThreshold = 0.1f; // valor mínimo de opacidade para ser clicável
